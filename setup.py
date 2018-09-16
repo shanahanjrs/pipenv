7da46e774b1b7b9203304abb76ae714260a8878a
John Shanahan <shanahan.jrs@gmail.com>
2018-09-15 23:18:44 -0400

    Added publish flag to the pipver script and changed the --tag flag to --dont-tag to be more consistent

--- setup.py
+++ setup.py
@@ -6,12 +6,6 @@ import sys
 
 from setuptools import setup
 
-# 'setup.py publish' shortcut.
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel')
-    os.system('twine upload dist/*')
-    sys.exit()
-
 requires = [
     'gitpython',
     'click'
