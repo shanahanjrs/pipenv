5647dba11dcc9d0702514f69f7c72313a036a8d1
John Shanahan <2022523+shanahanjrs@users.noreply.github.com>
2020-07-26 21:40:25 -0400

    Yapf py files (#1)
    * yapf all files
    
    * Remove setuppy since i missed it earlier

--- pipver/lib.py
+++ pipver/lib.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-
 """
 filelib.py
 """
@@ -11,8 +10,10 @@ import re
 # https://github.com/semver/semver/pull/460/files
 VALID_SEMVER = '(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?'
 
+
 def is_valid_semver(semver_string):
     return bool(re.match(VALID_SEMVER, semver_string))
+
 
 def possible_file_paths():
     """
@@ -29,19 +30,16 @@ def possible_file_paths():
     # Useful because we need to be in the package repos root dir
     # and the source should be in another dir with the same name
     pkg_name = os.path.basename(os.getcwd())
-    
+
     return [
         '%s/_version.py' % pkg_name,
-        '%s/VERSION'     % pkg_name,
-        '%s/VERSION.py'  % pkg_name,
+        '%s/VERSION' % pkg_name,
+        '%s/VERSION.py' % pkg_name,
         '%s/VERSION.txt' % pkg_name,
-        '%s/__init__.py' % pkg_name,
-        '_version.py',
-        'VERSION',
-        'VERSION.py',
-        'VERSION.txt',
-        'setup.py'
+        '%s/__init__.py' % pkg_name, '_version.py', 'VERSION', 'VERSION.py',
+        'VERSION.txt', 'setup.py'
     ]
+
 
 def search_for_possible_files(filepath=None):
     """
@@ -56,11 +54,12 @@ def search_for_possible_files(filepath=None):
 
     return files_that_exist
 
+
 def search_file_for_valid_semver(filepath):
     """
     Take a file and look for the version string
     """
-    ret = None 
+    ret = None
 
     with open(filepath, 'r') as f:
         for line in f.readlines():
@@ -71,15 +70,16 @@ def search_file_for_valid_semver(filepath):
             if m:
                 ret = m.group(0)
                 break
-    
+
     return ret
+
 
 def search_each_existing_file(files):
     """
     Takes a List of files that exist and searches for the first valid semver string, then
     returns the string and the name of the file it was found in
     """
-    ret =  None, None
+    ret = None, None
 
     for file in files:
         n = search_file_for_valid_semver(file)
@@ -90,6 +90,7 @@ def search_each_existing_file(files):
 
     return ret
 
+
 def modify_file_with_new_version_string(file, old, new):
     """
     Takes a file path and the old and new version strings to do a replacement
@@ -99,6 +100,6 @@ def modify_file_with_new_version_string(file, old, new):
 
     if old in file_contents:
         file_contents = file_contents.replace(old, new, 1)
-    
+
         with open(file, 'w') as f:
             f.write(file_contents)
